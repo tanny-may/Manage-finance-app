df4e74ad0b3a8c32ee0ca6689ff8c9baf1af0b9e
Tanya <tanya777fan@mail.ru>
2023-04-21 15:09:52 +0300

    transaction

--- public/js/api/Transaction.js
+++ public/js/api/Transaction.js
@@ -1,9 +1,6 @@
-/**
- * Класс Transaction наследуется от Entity.
- * Управляет счетами пользователя.
- * Имеет свойство URL со значением '/transaction'
- * */
-class Transaction extends Entity {
+/** Класс Transaction наследуется от Entity. Управляет счетами пользователя. Имеет свойство URL со значением '/transaction'*/
 
+class Transaction extends Entity {
+    static URL = '/transaction';
 }
 
