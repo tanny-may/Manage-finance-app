387bee8942b8da22604036fe16848a8b2c0eb07c
Tanya <tanya777fan@mail.ru>
2023-04-24 13:30:53 +0300

    register form

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -1,15 +1,10 @@
-/**
- * Класс RegisterForm управляет формой
- * регистрации
- * */
+/** Класс RegisterForm управляет формой регистрации */
 class RegisterForm extends AsyncForm {
-  /**
-   * Производит регистрацию с помощью User.register
-   * После успешной регистрации устанавливает
-   * состояние App.setState( 'user-logged' )
-   * и закрывает окно, в котором находится форма
-   * */
+  /** Производит регистрацию с помощью User.register После успешной регистрации устанавливает состояние App.setState( 'user-logged' ) и закрывает окно, в котором находится форма */
   onSubmit(data) {
-
+    User.register(data, (error, result) => {
+      if (result) App.setState('user-logged');
+    });
+    App.getModal('register').close();
   }
 }
