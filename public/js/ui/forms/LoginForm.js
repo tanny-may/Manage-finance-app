47a3f6b7bafe9d1f0ea420c1a0d33ff162cc826d
Tanya <tanya777fan@mail.ru>
2023-04-24 13:30:42 +0300

    login form

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -1,15 +1,10 @@
-/**
- * Класс LoginForm управляет формой
- * входа в портал
- * */
+/** Класс LoginForm управляет формой входа в портал */
 class LoginForm extends AsyncForm {
-  /**
-   * Производит авторизацию с помощью User.login
-   * После успешной авторизации, сбрасывает форму,
-   * устанавливает состояние App.setState( 'user-logged' ) и
-   * закрывает окно, в котором находится форма
-   * */
+  /** Производит авторизацию с помощью User.login После успешной авторизации, сбрасывает форму, устанавливает состояние App.setState( 'user-logged' ) и закрывает окно, в котором находится форма */
   onSubmit(data) {
-
+    User.login(data, (error, result) => {
+      if (result) App.setState('user-logged');
+    });
+    App.getModal('login').close();
   }
 }
