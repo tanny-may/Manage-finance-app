5f8d3b88d06b2c3bfc05f02e8d95e02d3e4dd639
Tanya <tanya777fan@mail.ru>
2023-04-24 13:30:33 +0300

    create account

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -1,14 +1,8 @@
-/**
- * Класс CreateAccountForm управляет формой
- * создания нового счёта
- * */
+/** Класс CreateAccountForm управляет формой создания нового счёта */
 class CreateAccountForm extends AsyncForm {
-  /**
-   * Создаёт счёт с помощью Account.create и закрывает
-   * окно в случае успеха, а также вызывает App.update()
-   * и сбрасывает форму
-   * */
+  /** Создаёт счёт с помощью Account.create и закрывает окно в случае успеха, а также вызывает App.update() и сбрасывает форму */
   onSubmit(data) {
-
+    Account.create(date, App.update);
+    Modal.close();
   }
 }
